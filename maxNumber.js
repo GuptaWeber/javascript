c4ccb52443d1f504891c544ef6cb9cb9821571ed
Gupta Weber <31372096+GuptaWeber@users.noreply.github.com>
2024-04-10 03:37:56 +0000

    feat(maxNumbers): added the maxNumbers func

---
+++ maxNumber.js
@@ -0,0 +1,19 @@
+function maxNum(arr){
+    if(!arr || arr?.length === 0){
+        return 0;
+    }
+
+    let max = arr[0];
+    
+    for(let i=1; i<arr.length; i++){
+        if(max < arr[i]){
+            max = arr[i]
+        }
+    }
+
+    return max;
+}
+
+const array = [10, 4, 100, 90, 150, 25];
+
+console.log(maxNum(array));
